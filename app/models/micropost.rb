4f094cfad77b15d7c5fe85d4172b41fdd8a0f5e0
vanvlaenderend <vanvlaenderendennis@gmail.com>
2013-12-05 13:16:22 +0100

    css/css3 toegepast

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,4 @@
 class Micropost < ActiveRecord::Base
-class Micropost < ActiveRecord::Base
 	attr_accessible :content, :user_id
 	validates_length_of :content, {:maximum => 25, :minimum => 8,}
 	validates_presence_of :user_id, {presence => true}
