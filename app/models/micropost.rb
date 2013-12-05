da93149f2e64a906381aaf30acc4dcf659c0138e
vanvlaenderend <vanvlaenderendennis@gmail.com>
2013-12-05 12:30:16 +0100

    Finish

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,6 @@
 class Micropost < ActiveRecord::Base
-  attr_accessible :content, :user_id
+class Micropost < ActiveRecord::Base
+	attr_accessible :content, :user_id
+	validates_length_of :content, {:maximum => 25, :minimum => 8,}
+	validates_presence_of :user_id, {presence => true}
 end
